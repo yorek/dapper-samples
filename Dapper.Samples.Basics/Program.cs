cfbdc80bc53e2d843c5bf04a17762fdda5ba42cc
yorek <info@davidemauri.it>
2017-11-20 15:41:24 -0800

    initial commit

---
+++ Dapper.Samples.Basics/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using Dapper;
+
+namespace Dapper.Samples.Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataFolder = Directory.GetParent(Environment.CurrentDirectory).GetDirectories("Dapper.Samples.Data").Single();
+
+            // Create connection string
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(LocalDB)\MSSQLLocalDB",
+                AttachDBFilename = $@"{dataFolder.FullName
+                }\DapperSample.mdf",
+                IntegratedSecurity = true,
+                ConnectTimeout = 30,
+                ApplicationName = "Dapper.Samples.Basics"
+
+            };
+
+            // Wrap common code in an Action shell
+            Action<string, Action<SqlConnection>> ExecuteSample = (message, action) =>
+            {
+                Console.WriteLine(message);
+                using (var conn = new SqlConnection(builder.ConnectionString))
+                {
+                    action(conn);
+                }
+                Console.WriteLine();                    
+            };
+
+            // Return a dynamic type list
+            ExecuteSample("1. Return a list of dynamic objects", conn =>
+            {
+                var queryResult = conn.Query("SELECT [Id], [FirstName], [LastName] FROM dbo.[Users]");
+                Console.WriteLine("{0} is a {1}", nameof(queryResult), queryResult.GetType());                
+                queryResult.ToList().ForEach(u => Console.WriteLine($"{u}"));                
+
+            });
+            
+            // Return a strongly typed list
+            ExecuteSample("2. Return a list of strongly typed objects", conn =>
+            {
+                var queryResult = conn.Query<User>("SELECT [Id], [FirstName], [LastName] FROM dbo.[Users]");
+                Console.WriteLine("{0} is a {1}", nameof(queryResult), queryResult.GetType());                
+                queryResult.ToList().ForEach(u => Console.WriteLine($"{u}"));                
+            });
+
+            // Return an untyped object
+            ExecuteSample("3. Return an object scalar", conn =>
+            {
+                object firstName = conn.ExecuteScalar("SELECT [FirstName] FROM dbo.[Users] WHERE [Id] = 1");
+                Console.WriteLine("{0} ({1}): {2}", nameof(firstName), firstName.GetType(), firstName);
+            });
+
+            // Return an typed object
+            ExecuteSample("4. Return a typed object", conn =>
+            {
+                var firstName = conn.ExecuteScalar<string>("SELECT [FirstName] FROM dbo.[Users] WHERE [Id] = 1");
+                Console.WriteLine("{0} ({1}): {2}", nameof(firstName), firstName.GetType(), firstName);
+            });
+
+            // Execute sample: return affected rows
+            ExecuteSample("5. Execute a statement and return affected rows. Any resultset will be discared.", conn =>
+            {
+                int affectedRows = conn.Execute("UPDATE dbo.[Users] SET [FirstName] = 'John' WHERE [Id] = 3");
+                Console.WriteLine("'UPDATE' Affected Rows: {0}", affectedRows);
+            });            
+            
+            // Parameters sample
+            ExecuteSample("6. Create and set parameter values using anonymous objects.", conn =>
+            {
+                var queryResult = conn.Query<User>("SELECT [Id], [FirstName], [LastName] FROM dbo.[Users] WHERE Id = @Id", new { @Id = 1 });
+                queryResult.ToList().ForEach(u => Console.WriteLine($"{u}"));                     
+            });            
+
+            ExecuteSample("7. Create and set parameter values and properties (type and size).", conn =>
+            {
+                DynamicParameters dp = new DynamicParameters();
+                dp.Add("FirstName", "Davide", DbType.String, ParameterDirection.Input, 100);
+                dp.Add("LastName", "Mauri");
+
+                var queryResult = conn.Query<User>("SELECT [Id], [FirstName], [LastName] FROM dbo.[Users] WHERE FirstName = @FirstName AND LastName = @LastName", dp);
+                queryResult.ToList().ForEach(u => Console.WriteLine($"{u}"));                          
+            });
+
+            // Stored procedure execution sample
+            ExecuteSample("8. Execute a stored procedure that returns user info.", conn =>
+            {
+                var queryResult = conn.Query<User>("dbo.ProcedureBasic", new { @email = "info@davidemauri.it" }, commandType: CommandType.StoredProcedure);
+
+                queryResult.ToList().ForEach(u => Console.WriteLine($"{u}"));                          
+            });
+
+            ExecuteSample("9. Execute a stored procedure that has input and output params and a return value.", conn =>
+            {
+                DynamicParameters dp = new DynamicParameters();
+                dp.Add("email", "info@davidemauri.it");
+                dp.Add("firstName", null, DbType.String, ParameterDirection.Output, 100); // Manually specify parameter details
+                dp.Add("lastName", "", direction: ParameterDirection.Output); // Infer type and size from given value
+                dp.Add("result", null, DbType.Int32, ParameterDirection.ReturnValue);
+
+                conn.Execute("dbo.ProcedureWithOutputAndReturnValue", dp, commandType: CommandType.StoredProcedure);
+
+                Console.WriteLine("User: {0}, {1}, {2}", dp.Get<int>("result"), dp.Get<string>("firstName"), dp.Get<string>("lastName"));                
+            });
+
+            ExecuteSample("10. Get a DataReader, if you really prefer.", conn => {
+                var dataReader = conn.ExecuteReader("SELECT [Id], [FirstName], [LastName] FROM dbo.[Users]");
+                while (dataReader.Read())
+                {
+                    Console.WriteLine("User: {0}, {1}, {2}", dataReader["Id"], dataReader["FirstName"], dataReader["LastName"]);
+                }                
+            });
+        }
+    }
+}
---
+++ Dapper.Samples.Basics/User.cs
@@ -0,0 +1,16 @@
+namespace Dapper.Samples.Basics
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }    
+
+        public string LastName { get; set; }
+
+        public override string ToString() 
+        {
+            return $"Id: {Id}, FirstName: {FirstName}, LastName: {LastName}";
+        }
+    }
+}
