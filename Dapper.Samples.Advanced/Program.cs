8cc732f59c49bcebe88e2fd885934ee843499de8
yorek <info@davidemauri.it>
2017-11-24 10:28:06 -0800

    added multiple execution sample

---
+++ Dapper.Samples.Advanced/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace Dapper.Samples.Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var dataFolder = Directory.GetParent(Environment.CurrentDirectory).GetDirectories("Dapper.Samples.Data").Single();
+
+            // Create connection string
+            var builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(LocalDB)\MSSQLLocalDB",
+                AttachDBFilename = $@"{dataFolder.FullName
+                }\DapperSample.mdf",
+                IntegratedSecurity = true,
+                ConnectTimeout = 30,
+                ApplicationName = "Dapper.Samples.Advanced"
+
+            };
+
+            // Wrap common code in an Action shell
+            Action<string, Action<SqlConnection>> ExecuteSample = (message, action) =>
+            {
+                Console.WriteLine(message);
+                using (var conn = new SqlConnection(builder.ConnectionString))
+                {
+                    action(conn);
+                }
+                Console.WriteLine();                    
+            };
+
+            ExecuteSample("Mulitple Execution", conn => MultipleExecution.ShowSample(conn));
+        }
+    }
+}
