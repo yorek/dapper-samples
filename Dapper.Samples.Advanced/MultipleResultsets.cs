cf4b246774a8445f4f6f4c952e0ec219dc525b94
yorek <info@davidemauri.it>
2017-12-10 17:05:56 -0800

    updated mulltiple resultsets sample

--- Dapper.Samples.Advanced/MultipleResultsets.cs
+++ Dapper.Samples.Advanced/MultipleResultsets.cs
@@ -14,36 +14,53 @@ namespace Dapper.Samples.Advanced
 
         public int Order => 2;
 
-        public class User
+        public class Customer
         {
             public int Id { get; private set; }
-            public string FirstName { get; private set; }
-            public string LastName { get; private set; }
+            public string Name { get; private set; }
+            public List<CustomerOrder> Orders => _orders;
+            public decimal TotalOrdersValue => _orders.Sum(o => o.OrderAmount);
+
+            private List<CustomerOrder> _orders = new List<CustomerOrder>();
 
             public override string ToString()
             {
-                return $"USER => Id: {Id}, FirstName: {FirstName}, LastName: {LastName}";
+                return $"CUSTOMER => Id: {Id}, Name: {Name}, TotalOrdersValue: {TotalOrdersValue}";
             }
         }
 
-        public class Company
+        public class CustomerOrder
         {
             public int Id { get; private set; }
-            public string CompanyName { get; private set; }
+            public DateTime OrderDate { get; private set; }
+            public decimal OrderAmount { get; private set; }
+            
             public override string ToString()
             {
-                return $"COMPANY => Id: {Id}, FirstName: {CompanyName}";
+                return $"ORDER => Id: {Id}, OrderDate: {OrderDate}, OrderAmout: {OrderAmount}";
             }
         }
 
         public void ShowSample(SqlConnection conn)
         {
-            var results = conn.QueryMultiple("SELECT Id, FirstName, LastName FROM dbo.Users; SELECT Id, CompanyName FROM dbo.Companies");
-            var users = results.Read<User>();
-            var companies = results.Read<Company>();
+            var results = conn.QueryMultiple(@"
+                SELECT Id, [Name] FROM dbo.Customers WHERE Id = @customerId; 
+                SELECT Id, [OrderDate], [OrderAmount] FROM dbo.Orders WHERE CustomerId = @customerId;
+                ",
+                new {
+                    @customerId = 1
+                });
 
-            users.ToList().ForEach(u => Console.WriteLine($"{u}"));     
-            companies.ToList().ForEach(c => Console.WriteLine($"{c}"));     
+            var customer = results.ReadSingle<Customer>();
+            var orders = results.Read<CustomerOrder>();
+
+            customer.Orders.AddRange(orders);
+
+            Console.WriteLine($"{customer}");     
+            foreach(var o in customer.Orders)
+            {
+                Console.WriteLine($"{o}");     
+            }
         }
     }
 }
