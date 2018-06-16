6223018c2531bad014ca69567d5699c5dcf6419a
yorek <info@davidemauri.it>
2018-06-16 15:45:35 -0700

    completed one-to-many sample

--- Dapper.Samples.Advanced/OneToMany.cs
+++ Dapper.Samples.Advanced/OneToMany.cs
@@ -22,100 +22,30 @@ namespace Dapper.Samples.Advanced
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string EmailAddress { get; set; }
-            public JArray Tags { get; set; }
-            public Roles Roles { get; set; }
-            public Company Company { get; set; }
-            public Preferences Preferences { get; set; }
+            public Tags Tags { get; set; }
 
             public override string ToString()
             {
-                return String.Format($"({Id}) {FirstName},{LastName},{EmailAddress}");
+                return String.Format($"User: ({Id}) {FirstName},{LastName},{EmailAddress} ({Tags})");
             }
         }
 
-        public class Company
+        public class Tag
         {
-            public int Id { get; set; }
-            public string CompanyName { get; set; }
-            public Address Address { get; set; }
+            public string Name { get; set; }
 
             public override string ToString()
             {
-                return String.Format($"({Id}) {CompanyName}");
+                return String.Format($"{Name}");
             }
+
         }
 
-        public class Address
-        {
-            public string Street { get; set; }
-            public string City { get; set; }
-            public string State { get; set; }
-            public string Country { get; set; }
-
-            public override string ToString()
-            {
-                string[] a = new string[] { Street, City, State, Country };
-
-                var r = Array.FindAll<string>(a, s => !string.IsNullOrEmpty(s));
-
-                return String.Format(string.Join(",", r));
-            }
-        }
-
-        public class Role
-        {
-            public string RoleName { get; set; }
-
-            public Role() { }
-
-            public Role(string name)
-            {
-                RoleName = name;
-            }
-
-            public override string ToString()
-            {
-                return RoleName;
-            }
-        }
-
-        public class Roles : List<Role>
+        public class Tags: List<Tag>
         {
             public override string ToString()
             {
-                return string.Join("|", this);
-            }
-
-            public static Roles FromString(string value)
-            {
-                Roles result = new Roles();
-
-                string[] roles = value.ToString().Split('|');
-
-                result.AddRange(roles.Select(r => new Role(r)));
-
-                return result;
-            }
-        }
-
-        public class Preferences
-        {
-            public string Theme;
-            public string Style;
-            public string Resolution;
-        }
-
-        public class JArrayTypeHandler : SqlMapper.TypeHandler<JArray>
-        {
-            public override JArray Parse(object value)
-            {
-                string json = value.ToString();
-                return JArray.Parse(value.ToString());
-            }
-
-            public override void SetValue(IDbDataParameter parameter, JArray value)
-            {
-                parameter.Value = value.ToString();
+                return String.Format(string.Join(",", this));
             }
         }
 
@@ -141,40 +71,13 @@ namespace Dapper.Samples.Advanced
             Console.WriteLine();
 
             SqlMapper.ResetTypeHandlers();
-            SqlMapper.AddTypeHandler(new JArrayTypeHandler());
+            SqlMapper.AddTypeHandler(new UserTypeHandler());
 
-            User u = new User()
-            {
-                FirstName = "Davide",
-                LastName = "Mauri",
-                EmailAddress = "davide.mauri@gmail.com",
-                Tags = new JArray() { "alpha", "beta" },
-                Roles = new Roles()
-                {
-                    new Role() { RoleName = "User" },
-                    new Role() { RoleName = "Developer" },
-                    new Role() { RoleName = "Administrator"}
-                },
-                Preferences = new Preferences()
-                {
-                    Resolution = "1920x1080",
-                    Style = "Black",
-                    Theme = "Modern"
-                },
-                Company = new Company()
-                {
-                    CompanyName = "Microsoft Corp.",
-                    Address = new Address()
-                    {
-                        City = "Redmond",
-                        State = "WA",
-                        Country = "United States",
-                        Street = "16225 NE 87th Street"
-                    }
-                }
-            };
+            var users = conn.Query<User>("dbo.GetUsersJson", commandType: CommandType.StoredProcedure);
+            users.ToList().ForEach(u => {
+                Console.WriteLine(u);                                
+            });
 
-            //var executeResult = conn.Set<User>(u);
             Console.WriteLine();
         }
     }
